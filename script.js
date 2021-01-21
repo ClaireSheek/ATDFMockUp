b8a36ea60b022ad05bbc50b6570218f29277684c
Claire Sheek <48499010+ClaireSheek@users.noreply.github.com>
2021-01-20 21:22:35 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// $(document).ready(function() {
+//   $('a[anchor]').click(function(e) {
+//       e.preventDefault();
+//       var anchor = $(this).attr('anchor');
+//       var container = $(.mainContainer)
+//       var top = $('div[name='+anchor+']').offset().top;
+//       $('html, body', container).animate({scrollTop:top}, 300);
+//   });
+// });
